1fe03c146654ff5bee706bf92e7228b7743d2ca1
hadi.fahriza <hadi.fahriza@caditglobal.com>
2023-06-08 10:45:04 +0700

    init main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_forecast/app.dart';
+import 'package:weather_forecast/injection.dart' as di;
 
-void main() {
+void main() async {
+  di.init();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
